38e121a53419d5c1fd090e52f1ea5fadde6c3797
thedavidmeister <thedavidmeister@gmail.com>
2023-02-28 17:47:57 +0400

    devcontainer

--- devenv.nix
+++ devenv.nix
@@ -11,16 +11,21 @@
   scripts.hello.exec = "echo hello from $GREET";
 
   enterShell = ''
-    hello
-    git --version
+    export CARGO_INSTALL_ROOT="$PWD/.cargo"
+    export PATH="$PWD/.cargo/bin:$PATH"
+    cargo install --git https://github.com/foundry-rs/foundry foundry-cli anvil chisel --bins --locked
   '';
 
   # https://devenv.sh/languages/
   # languages.nix.enable = true;
+  languages.rust.enable = true;
 
   # https://devenv.sh/pre-commit-hooks/
   # pre-commit.hooks.shellcheck.enable = true;
 
   # https://devenv.sh/processes/
   # processes.ping.exec = "ping example.com";
+
+  # https://devenv.sh/integrations/codespaces-devcontainer/
+  devcontainer.enable = true;
 }
