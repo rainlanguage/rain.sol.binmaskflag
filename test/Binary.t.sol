997b9397ae07ac965548e28c39cbf22c35545539
Aishwarya Kendle <47445489+SocioDroid@users.noreply.github.com>
2023-03-15 11:45:23 +0000

    Binary Masks tests wip

--- test/Binary.t.sol
+++ test/Binary.t.sol
@@ -8,5 +8,19 @@ contract BinaryTest is Test {
     function testBConstants() public {
         assertEq(B_1, 1);
         assertEq(B_11, 3);
+        assertEq(B_111, 7);
+        assertEq(B_1111, 15);
+        assertEq(B_11111, 31);
+        assertEq(B_111111, 63);
+        assertEq(B_1111111, 127);
+        assertEq(B_11111111, 255);
+        assertEq(B_111111111, 511);
+        assertEq(B_1111111111, 1023);
+        assertEq(B_11111111111, 2047);
+        assertEq(B_111111111111, 4095);
+        assertEq(B_1111111111111, 8191);
+        assertEq(B_11111111111111, 16383);
+        assertEq(B_111111111111111, 32767);
+        assertEq(B_1111111111111111, 65535);
     }
 }
